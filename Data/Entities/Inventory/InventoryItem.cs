30d18dc8169a327baa7ddc1c9d6c35b49545a72d
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-05-30 16:02:44 +0300

    Add InventoryItem class for inventory management
    Introduces the `InventoryItem` class in the
    `WarehouseManagementSystem.Data.Entities.Inventory` namespace.

---
+++ Data/Entities/Inventory/InventoryItem.cs
@@ -0,0 +1,35 @@
+﻿using WarehouseManagementSystem.Data.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem.Data.Entities.Inventory
+{
+    public class InventoryItem
+    {
+        [ForeignKey("Warehouse")]
+        public int WarehouseID { get; set; }
+
+        [MaxLength(30)]
+        [ForeignKey("Item")]
+        public string? ItemCode { get; set; }
+
+        [Required]
+        public DateTime? ProductionDate { get; set; }
+
+        [Required]
+        public DateTime? ExpiryDate { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        // Navigation properties
+        public virtual Warehouse? Warehouse { get; set; }
+        public virtual Item? Item { get; set; }
+    }
+}

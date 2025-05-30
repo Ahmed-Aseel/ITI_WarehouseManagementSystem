673ae86ca45a8c6a04c7b565437729e1401718ef
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-05-30 16:07:42 +0300

    Add PermissionItem class and update Transfer class
    Introduced the `PermissionItem` class.
    Updated the `Transfer` class.

---
+++ Data/Entities/Transfers/PermissionItem.cs
@@ -0,0 +1,31 @@
+﻿using WarehouseManagementSystem.Data.Entities.Core;
+using WarehouseManagementSystem.Data.Entities.Permissions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem.Data.Entities.Transfers
+{
+    public class PermissionItem
+    {
+        [MaxLength(30)]
+        [ForeignKey("Permission")]
+        public string? PermissionNumber { get; set; }
+
+        [MaxLength(30)]
+        [ForeignKey("Item")]
+        public string? ItemCode { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        // Navigation properties
+        public virtual Permission? Permission { get; set; }
+        public virtual Item? Item { get; set; }
+    }
+}
---
+++ Data/Entities/Transfers/Transfer.cs
@@ -0,0 +1,41 @@
+﻿using WarehouseManagementSystem.Data.Entities.Core;
+using WarehouseManagementSystem.Data.Entities.Permissions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem.Data.Entities.Transfers
+{
+    public class Transfer
+    {
+        public Transfer()
+        {
+            TransferPermissions = new HashSet<TransferPermission>();
+        }
+
+        [Key]
+        [MaxLength(30)]
+        public string? TransferNumber { get; set; }
+
+        [Required]
+        public DateTime? TransferDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [ForeignKey("SourceWh")]
+        public int SrcWhID { get; set; }
+
+        [Required]
+        [ForeignKey("DestinationWh")]
+        public int DestWhID { get; set; }
+
+        // Navigation properties
+        public virtual Warehouse? SourceWh { get; set; }
+        public virtual Warehouse? DestinationWh { get; set; }
+        public virtual ICollection<TransferPermission> TransferPermissions { get; set; }
+    }
+}

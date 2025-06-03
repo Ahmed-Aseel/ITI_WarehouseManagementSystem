135391797c6a4fcdfcc50abbce04e56bf8d079d1
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-06-03 22:13:44 +0300

    Add entry point and update project configuration
    Introduced a new `Program` class in `Program.cs` as the application entry point, initializing the configuration and launching `MainForm`. Updated `WarehouseManagementSystem.csproj` to target .NET 9.0, enable Windows Forms, and include necessary Entity Framework Core packages.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using WarehouseManagementSystem.Forms;
+
+namespace WarehouseManagementSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}

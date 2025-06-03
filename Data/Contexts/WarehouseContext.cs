e1cd931afce2c4da7f73d39d2f386d1f77a61aae
Ahmed-Aseel <ahmedaseel155@gmail.com>
2025-06-03 22:06:38 +0300

    Refactor WarehouseContext and update entity relationships
    Removed the Transfer DbSet and related configurations from the WarehouseContext. Simplified the constructor by eliminating unnecessary braces. Updated entity relationships for Warehouse, SupplyPermission, ReleasePermission, and TransferPermission, and clarified comments regarding these relationships.

--- Data/Contexts/WarehouseContext.cs
+++ Data/Contexts/WarehouseContext.cs
@@ -2,19 +2,16 @@
 using WarehouseManagementSystem.Data.Entities.Core;
 using WarehouseManagementSystem.Data.Entities.Inventory;
 using WarehouseManagementSystem.Data.Entities.Permissions;
-using WarehouseManagementSystem.Data.Entities.Transfers;
 
 namespace WarehouseManagementSystem.Data.Contexts
 {
     public class WarehouseContext : DbContext
     {
-        public WarehouseContext()
-        {
-        }
+        public WarehouseContext() { }
 
-        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
-        {
-        }
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options) { }
+
+        // DbSets for each entity
         public DbSet<Warehouse> Warehouses { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<InventoryItem> InventoryItems { get; set; }
@@ -23,7 +20,6 @@ namespace WarehouseManagementSystem.Data.Contexts
         public DbSet<SupplyPermission> SupplyPermissions { get; set; }
         public DbSet<ReleasePermission> ReleasePermissions { get; set; }
         public DbSet<TransferPermission> TransferPermissions { get; set; }
-        public DbSet<Transfer> Transfers { get; set; }
         public DbSet<PermissionItem> PermissionItems { get; set; }
 
         override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -60,121 +56,99 @@ namespace WarehouseManagementSystem.Data.Contexts
             {
                 table.HasCheckConstraint(
                     "CK_Permission_Type",
-                    $"[Type] IN ({(byte)PermissionType.Supply}, {(byte)PermissionType.Dispense}, {(byte)PermissionType.Transfer})"
+                    $"[Type] IN ({(byte)PermissionType.Supply}, {(byte)PermissionType.Release}, {(byte)PermissionType.Transfer})"
                 );
             });
 
             modelBuilder.Entity<SupplyPermission>().ToTable("SupplyPermissions");
             modelBuilder.Entity<ReleasePermission>().ToTable("ReleasePermissions");
 
-            modelBuilder.Entity<TransferPermission>().ToTable("TransferPermissions", table =>
-            {
-                table.HasCheckConstraint(
-                    "CK_TransferPermission_Direction",
-                    $"[Direction] IN ({(byte)TransferDirection.Outgoing}, {(byte)TransferDirection.Incoming})"
-                );
-            });
-
-            modelBuilder.Entity<Transfer>().ToTable("Transfers", table =>
-            {
-                table.HasCheckConstraint("CK_Transfer_SrcDest_Wh", "[SrcWhID] <> [DestWhID]");
-            });
+            modelBuilder.Entity<TransferPermission>().ToTable("TransferPermissions");
 
             modelBuilder.Entity<PermissionItem>().ToTable("PermissionItems", table =>
             {
                 table.HasCheckConstraint("CK_PermissionItem_Quantity", "[Quantity] > 0");
             });
-            modelBuilder.Entity<PermissionItem>()
-                .HasKey(pi => new { pi.PermissionNumber, pi.ItemCode });
             #endregion
 
             #region Relationships
-            // Warehouse ↔ InventoryRecord
+            // Warehouse ↔ InventoryRecord (one-to-many)
             modelBuilder.Entity<Warehouse>()
                 .HasMany(w => w.Inventories)
                 .WithOne(ir => ir.Warehouse)
                 .HasForeignKey(ir => ir.WarehouseID)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // Item ↔ InventoryRecord
+            // Item ↔ InventoryRecord (one-to-many)
             modelBuilder.Entity<Item>()
                 .HasMany(i => i.Inventories)
                 .WithOne(ir => ir.Item)
                 .HasForeignKey(ir => ir.ItemCode)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // BusinessPartner ↔ Permissions
+            // BusinessPartner ↔ SupplyPermissions (one-to-many)
             modelBuilder.Entity<BusinessPartner>()
                 .HasMany(bp => bp.SupplyPermissions)
                 .WithOne(sp => sp.Supplier)
                 .HasForeignKey(sp => sp.SupplierID)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            // BusinessPartner ↔ ReleasePermissions
+            // BusinessPartner ↔ ReleasePermissions (one-to-many)
             modelBuilder.Entity<BusinessPartner>()
                 .HasMany(bp => bp.ReleasePermissions)
                 .WithOne(dp => dp.Customer)
                 .HasForeignKey(dp => dp.CustomerID)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // Permission ↔ Warehouse
-            modelBuilder.Entity<Permission>()
-                .HasOne(p => p.Warehouse)
-                .WithMany(w => w.Permissions)
-                .HasForeignKey(p => p.WarehouseID)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // Permission ↔ SupplyPermission/ReleasePermission/TransferPermission (One-to-One)
+            // Warehouse (Main Warehouse) ↔ Permission (one-to-many)
+            modelBuilder.Entity<Warehouse>()
+                .HasMany(w => w.Permissions)
+                .WithOne(p => p.MainWarehouse)
+                .HasForeignKey(p => p.MainWarehouseID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Warehouse (Destination Warehouse) ↔ TransferPermission (one-to-many)
+            modelBuilder.Entity<Warehouse>()
+                .HasMany(w => w.Transfers)
+                .WithOne(tp => tp.DestWarehouse)
+                .HasForeignKey(tp => tp.DestWarehouseID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // SupplyPermission ↔ Permission (One-to-One)
             modelBuilder.Entity<SupplyPermission>()
                 .HasOne(sp => sp.Permission)
                 .WithOne(p => p.SupplyPermission)
                 .HasForeignKey<SupplyPermission>(sp => sp.PermissionNumber)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // ReleasePermission ↔ Permission
+            // ReleasePermission ↔ Permission (One-to-One)
             modelBuilder.Entity<ReleasePermission>()
                 .HasOne(dp => dp.Permission)
                 .WithOne(p => p.ReleasePermission)
                 .HasForeignKey<ReleasePermission>(dp => dp.PermissionNumber)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // TransferPermission ↔ Permission
+            // TransferPermission ↔ Permission (One-to-One)
             modelBuilder.Entity<TransferPermission>()
                 .HasOne(tp => tp.Permission)
                 .WithOne(p => p.TransferPermission)
                 .HasForeignKey<TransferPermission>(tp => tp.PermissionNumber)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // Transfer ↔ Warehouses
-            modelBuilder.Entity<Transfer>()
-                .HasOne(t => t.SourceWh)
-                .WithMany(w => w.OutgoingTransfers)
-                .HasForeignKey(t => t.SrcWhID)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // Transfer ↔ Destination Warehouse
-            modelBuilder.Entity<Transfer>()
-                .HasOne(t => t.DestinationWh)
-                .WithMany(w => w.IncomingTransfers)
-                .HasForeignKey(t => t.DestWhID)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // PermissionItem ↔ Permission & Item
-            modelBuilder.Entity<PermissionItem>()
-                .HasOne(pi => pi.Permission)
-                .WithMany(p => p.PermissionItems)
+            // Permission ↔PermissionItem (one-to-many)
+            modelBuilder.Entity<Permission>()
+                .HasMany(p => p.PermissionItems)
+                .WithOne(pi => pi.Permission)
                 .HasForeignKey(pi => pi.PermissionNumber)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            // PermissionItem ↔ Item
-            modelBuilder.Entity<PermissionItem>()
-                .HasOne(pi => pi.Item)
-                .WithMany(i => i.PermissionItems)
+            // Item ↔ PermissionItem (one-to-many)
+            modelBuilder.Entity<Item>()
+                .HasMany(i => i.PermissionItems)
+                .WithOne(pi => pi.Item)
                 .HasForeignKey(pi => pi.ItemCode)
                 .OnDelete(DeleteBehavior.Restrict);
             #endregion
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
